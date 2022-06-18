95789dfe0378c33a0e9751bf2d051c1108fbea76
samantha <langridge.se@gmail.com>
2022-06-18 15:44:59 -0600

    added requirements, updated client and client login

--- endpoints/client.py
+++ endpoints/client.py
@@ -1,9 +1,42 @@
 from app import app
-from flask import jsonify
+from flask import request, jsonify
 from helpers.dbhelpers import run_query
-
-@app.get('/client')
-def user_get():
-    return jsonify("endpoint"), 200
+import uuid
 
 
+# client get request
+@app.get('/api/client')
+def client_get():
+    client_list = run_query("SELECT * FROM client")
+    resp = []
+    for client in client_list:
+        client_obj= {}
+        client_obj["clientId"] = client[0]
+        client_obj["email"] = client[1]        
+        client_obj["username"] = client[2]
+        client_obj["firstName"] = client[4]
+        client_obj["lastName"] = client[5]
+        client_obj["createdAt"] = client[6]        
+        client_obj["pictureUrl"] = client[7]
+        resp.append(client_obj)
+    return jsonify(client_list), 200
+
+# client post request
+@app.post('/api/client')
+def client_post():
+    data = request.json
+    email = data.get("email")
+    username = data.get("username")
+    firstName = data.get("firstName")
+    lastName = data.get("lastName")
+    pictureUrl = data.get("pictureUrl")
+    if not email:
+        return jsonify ("Missing required argument : email"), 422
+    if not username:
+        return jsonify ("Missing required argument : username"), 422
+    if not firstName:
+        return jsonify ("Missing required argument : first name"), 422
+    if not lastName:
+        return jsonify ("Missing required argument : last name)"), 422
+    run_query("INSERT INTO client (email, username, firstName, lastName, pictureUrl) VALUES (?,?,?,?,?)", [email, username, firstName, lastName, pictureUrl])
+    return jsonify("Client added successfully"), 201
--- endpoints/client_login.py
+++ endpoints/client_login.py
@@ -1,7 +1,15 @@
 from app import app
-from flask import jsonify
+from flask import jsonify, request
 from helpers.dbhelpers import run_query
 
-@app.get('/client')
-def user_get():
-    return jsonify("endpoint"), 200
+@app.post('/api/client-login')
+def client_login():
+    data = request.json
+    email = data.get("email")
+    password = data.get("password")
+    if not email:
+        return jsonify ("Missing required argument : email"), 422
+    if not password:
+        return jsonify ("Missing required argument : password"), 422
+    run_query("INSERT INTO client (email, username, firstName, lastName, pictureUrl) VALUES (?,?,?,?,?)", [email, username, firstName, lastName, pictureUrl])
+    return jsonify("Client added successfully"), 201
